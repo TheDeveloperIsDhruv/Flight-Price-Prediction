3c6bd3f19e36a9bdf2b3a97566e2b632646fb33f
TheDeveloperIsDhruv <108569661+TheDeveloperIsDhruv@users.noreply.github.com>
2024-09-27 14:26:38 -0400

    successfully printing the values from the frontend page

--- flight.py
+++ flight.py
@@ -9,14 +9,26 @@ def welcome():
 msg=[]
 @app.route('/main',methods=['POST','GET'])
 def homepage():
-    if(request.method=="GET"):
+    if (request.method == "GET"):
         return render_template("main.html")
     else:
-        dest=request.form.get("dest")
-        source=request.form.get("source")
-        stops=request.form.get("stop")
-        airline=request.form.get("airline")
-        msg="Submitted!"
-        return render_template('main.html', message=msg)
+        departed_date = request.form.get('Ddate')
+        arrival_date = request.form.get('Adate')
+        source = request.form.get('source')
+        destination = request.form.get('dest')
+        stops=request.form.get('stop')
+        airline=request.form.get('airline')
+
+        # Print received values to the console
+        print(f"Departed date: {departed_date}")
+        print(f"Arrival date: {arrival_date}")
+        print(f"Source: {source}")
+        print(f"Destination: {destination}")
+        print(f"Number of stops: {stops}")
+        print(f"Airline: {airline}")
+        message='Submitted!'
+        return render_template("main.html",msg=message)
+    
+
 if __name__=="__main__":
     app.run(debug=True)
